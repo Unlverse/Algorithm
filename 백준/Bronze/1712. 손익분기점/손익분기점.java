8a312471da8e5979c164545d76beb59db9e36dcd
Jaewoo Choi <jwoochoi1123@gmail.com>
2025-11-29 03:01:12 +0900

    [Bronze II] Title: 손익분기점, Time: 1516 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+        int n = 1;
+
+        if (B >= C) {
+            System.out.println(-1);
+        } else {
+            while (true) {
+                if (A + (B * n) < C * n) {
+                    System.out.println(n);
+                    break;
+                }
+                n++;
+            }
+        }
+    }
+}
